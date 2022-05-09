a66ce9a6ed499028e781666141a5bc22bde18da0
SofiaBerezina <95140523+SofiaBerezina@users.noreply.github.com>
2022-05-09 13:59:00 +0500

    Add files via upload

---
+++ SQLalchemy-main/alchemy/data/users.py
@@ -0,0 +1,25 @@
+import datetime
+import sqlalchemy
+from alchemy.data.db_session import SqlAlchemyBase
+from sqlalchemy import orm
+from alchemy.data.news import News
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
+    password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    position = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    speciality = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    address = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
